0545ec596a87b41fbfaf464c846173417ec10e14
SilverFlin <toledorusso@outlook.com>
2023-09-07 23:09:41 -0700

    chore: add as03 instructions

---
+++ As03_LuisToledo/index.js
@@ -0,0 +1,7 @@
+// Hacer un programa que pueda registrar, modificar, eliminar y listar de alumnos y carreras (la especificación de las entidades está en la imagen de abajo), con las siguientes características.
+
+//     Que permita ver un listado de alumnos y de carreras (por separado)
+//     Debe contar con un menú en el que se pueda elegir la entidad y luego que permita decidir si se va a agregar, borrar o cambiar.
+//     Debe existir una manera de asignar un alumno a una carrera.
+//     Ordenar su código en clases y módulos.
+//     Utilizar el manejo asíncrono para facilitar el desarrollo.
